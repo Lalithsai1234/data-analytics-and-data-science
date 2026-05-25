44b17bdf65790e80f93e664541b866f4f0fd5f53
Lalith Sai <lalithsaimeesala123@gmail.com>
2026-05-25 11:30:14 +0530

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # data-analytics-and-data-science
+
+<!-- Animated header -->
+![Animated Header](assets/animated-header.svg)
+
+A curated collection of Python exercises and small projects focused on data analytics and data science topics.
+
+<!-- Animated Python files progress -->
+![Python Files Progress](assets/python-files-bar.svg)
+
+Repository snapshot:
+
+- **Python files:** 63
+- **Main folder:** `python/`
+
+Explore the `python/` folder for day-by-day lessons and tasks.
--- python/day54_pdpc.py
+++ python/day54_pdpc.py
@@ -1,73 +1,73 @@
 import mysql.connector
-# try:
-#     con=None
-#     csr=None
-#     con=mysql.connector.connect(
-#         user='root',
-#         password='R76s85l04u13@',
-#         database='pdpc',
-#         host='localhost',
-#         port='30000'
-#     )
+try:
+    con=None
+    csr=None
+    con=mysql.connector.connect(
+        user='root',
+        password='R76s85l04u13@',
+        database='pdpc',
+        host='localhost',
+        port='30000'
+    )
 
-#     csr=con.cursor()
-#     Query="""Insert into Employee values(1, "sai"),(2,"Lalith"); """
-#     csr.execute(Query)
-#     con.commit()
-# except Exception as e:
-#     print(e)
-# finally:
-#     if con!=None and csr!=None:
-#         csr.close()
-#         con.close()
+    csr=con.cursor()
+    Query="""Insert into Employee values(1, "sai"),(2,"Lalith"); """
+    csr.execute(Query)
+    con.commit()
+except Exception as e:
+    print(e)
+finally:
+    if con!=None and csr!=None:
+        csr.close()
+        con.close()
 
-# try:
-#     con=None
-#     csr=None
-#     con=mysql.connector.connect(
-#         user='root',
-#         password='R76s85l04u13@',
-#         database='pdpc',
-#         host='localhost',
-#         port='30000'
-#     )
+try:
+    con=None
+    csr=None
+    con=mysql.connector.connect(
+        user='root',
+        password='R76s85l04u13@',
+        database='pdpc',
+        host='localhost',
+        port='30000'
+    )
 
-#     csr=con.cursor()
-#     Query="""Insert into Employee values(%s, %s); """ #we can also write {id},{name} but we need to assign id and name above 
-#     id=int(input("Enter the Id:"))
-#     name=input("Enter the Name:")
-#     csr.execute(Query,(id,name)) #their should be a comma in between Query and Brackets
-#     con.commit()
-# except Exception as e:
-#     print(e)
-# finally:
-#     if con!=None and csr!=None:
-#         csr.close()
-#         con.close()
+    csr=con.cursor()
+    Query="""Insert into Employee values(%s, %s); """ #we can also write {id},{name} but we need to assign id and name above 
+    id=int(input("Enter the Id:"))
+    name=input("Enter the Name:")
+    csr.execute(Query,(id,name)) #their should be a comma in between Query and Brackets
+    con.commit()
+except Exception as e:
+    print(e)
+finally:
+    if con!=None and csr!=None:
+        csr.close()
+        con.close()
 
 
-# try:
-#     con=None
-#     csr=None
-#     con=mysql.connector.connect(
-#         user='root',
-#         password='R76s85l04u13@',
-#         database='pdpc',
-#         host='localhost',
-#         port='30000'
-#     )
+try:
+    con=None
+    csr=None
+    con=mysql.connector.connect(
+        user='root',
+        password='R76s85l04u13@',
+        database='pdpc',
+        host='localhost',
+        port='30000'
+    )
 
-#     csr=con.cursor()
-#     Query="""Insert into Employee values(%s, %s); """ #we can also write {id},{name} but we need to assign id and name above 
-#     data=[(4,'syam'), (5,'loki')]
-#     csr.executemany(Query,data) #their should be a comma in between Query and Brackets
-#     con.commit()
-# except Exception as e:
-#     print(e)
-# finally:
-#     if con!=None and csr!=None:
-#         csr.close()
-#         con.close()
+    csr=con.cursor()
+    Query="""Insert into Employee values(%s, %s); """ #we can also write {id},{name} but we need to assign id and name above 
+    data=[(4,'syam'), (5,'loki')]
+    csr.executemany(Query,data) #their should be a comma in between Query and Brackets
+    con.commit()
+except Exception as e:
+    print(e)
+finally:
+    if con!=None and csr!=None:
+        csr.close()
+        con.close()
 
 try:
     con=None
